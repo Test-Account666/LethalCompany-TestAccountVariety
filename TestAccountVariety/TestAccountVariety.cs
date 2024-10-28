dc901866d0603d068585cf1e48ca6f283c5ce71c
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2024-10-28 14:37:55 +0100

    Set minimum required version for TestAccountCore to '1.9.0'

--- TestAccountVariety/TestAccountVariety.cs
+++ TestAccountVariety/TestAccountVariety.cs
@@ -13,7 +13,7 @@ namespace TestAccountVariety;
 
 [BepInDependency("TestAccount666.DoorBreach", BepInDependency.DependencyFlags.SoftDependency)]
 [BepInDependency("evaisa.lethallib")]
-[BepInDependency("TestAccount666.TestAccountCore", "1.7.0")]
+[BepInDependency("TestAccount666.TestAccountCore", "1.9.0")]
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class TestAccountVariety : BaseUnityPlugin {
     public static TestAccountVariety Instance { get; private set; } = null!;
