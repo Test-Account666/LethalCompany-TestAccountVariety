311ddd38d223c4be8b6da2a27144eeb61928d59e
TestAccount666 <36412486+Test-Account666@users.noreply.github.com>
2025-02-20 22:33:31 +0100

    Remove redundant imports

--- TestAccountVariety/Items/ExtendedItems/LitGrabbableObject.cs
+++ TestAccountVariety/Items/ExtendedItems/LitGrabbableObject.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 namespace TestAccountVariety.Items.ExtendedItems;
 
--- TestAccountVariety/Items/ExtendedItems/LitNoisemakerProp.cs
+++ TestAccountVariety/Items/ExtendedItems/LitNoisemakerProp.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace TestAccountVariety.Items.ExtendedItems;
 
